eefd44a34412e5493691f62861c7f717439d60a4
Peter Wetzel <pdwetz@gmail.com>
2019-10-14 15:32:36 -0400

    netframework updated to 4.8 and nuget updated.

--- FileOrganizer/Properties/AssemblyInfo.cs
+++ FileOrganizer/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@
 [assembly: AssemblyDescription("Moves files to folders by loosely matching names")]
 [assembly: AssemblyConfiguration("FileOrganizer")]
 [assembly: AssemblyProduct("FileOrganizer")]
-[assembly: AssemblyCopyright("Copyright © 2018 Peter Wetzel")]
+[assembly: AssemblyCopyright("Copyright © 2019 Peter Wetzel")]
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
 /*
  * Version 1.3.0.0
  * - Minor refresh for updating to latest and greatest framework + nuget, plus some cleanup and adjustments
